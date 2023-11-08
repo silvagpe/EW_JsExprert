8fc41eeee874a5ec566bfd35fc6236343de3f6b0
Rafael Da Silva <silvagpe@gmail.com>
2023-11-08 20:33:21 -0300

    base structure

---
+++ modulo01/aula05-ddd/src/services/CarService.js
@@ -0,0 +1,45 @@
+const BaseRepository = require('./../repository/base/baseRepository')
+const CarCategory = require('./../entities/carCategory')
+
+class CarService {
+    constructor({ cars }) {
+        this.carRepository = new BaseRepository({ file: cars })
+    }
+
+    /**
+     * Retorna uma posição randomica do array
+     * @param {Array} list 
+     */
+    getRandomPositionFromArray(list){
+        const listLength = list.length
+        return Math.floor(
+            Math.random() * listLength
+        )
+    }
+
+    /**
+     * 
+     * @param {CarCategory} carCategory 
+     */
+    chooseRandomCar(carCategory){
+        
+        const randomCarIndex = this.getRandomPositionFromArray(carCategory.cardIds)
+        const carId = carCategory.cardIds[randomCarIndex]
+
+        return carId
+    }
+
+    /**
+     * 
+     * @param {CarCategory} carCategory 
+     */
+    async getAvaliableCar(carCategory){
+        const carId = this.chooseRandomCar(carCategory)
+        const car = await this.carRepository.find(carId)
+
+        return car
+    }
+
+
+}
+module.exports = CarService
