1070ad21f22feb2565e8df9a0eaaa44efb0daa16
Rafael Da Silva <silvagpe@gmail.com>
2023-12-17 21:15:36 -0300

    casos de usos de regex

--- modulo05/aula02-projeto01/src/textProcessorFluentAPI.js
+++ modulo05/aula02-projeto01/src/textProcessorFluentAPI.js
@@ -34,6 +34,18 @@ class TextProcessorFluentAPI {
 
         return this
     }
+    divideTextInColumns() {
+        const splitRegex = evaluateRegex(/,/)
+        this.#content = this.#content.map(line => line.split(splitRegex))
+
+        return this
+    }
+    removeEmptyCharacters() {
+        const trimSpaces = evaluateRegex(/^\s+|\s+$|\n/g)
+        this.#content = this.#content.map(line => line.map(item => item.replace(trimSpaces, "")))
+        
+        return this
+    }
     build() {
         return this.#content
     }
